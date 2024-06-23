93bd9a07867c6d54a9b285e1a8b16e23b915c56d
ayesha-fortheye <iamayesha42@gmail.com>
2024-06-23 16:20:31 +0530

    changes

---
+++ hotspots.js
@@ -0,0 +1,56 @@
+ const hotspots = [
+            {
+                position: new THREE.Vector3(-0.454843570379438, 0.5486758751955135, 0.7003607335382862),
+                name: "Anchor",
+                description: "About Anchors.",
+                imageUrl: "images/user-icon.png"
+            },
+            {
+                position: new THREE.Vector3(-2.9174817390140615, 0.03309830030057642, -3.346005526576052),
+                name: "Pin1",
+                description: "Pin Place 1.",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(3.112968130101585, 0.03309805866787374, -1.8628744936052188),
+                name: "Pin2",
+                description: "Pin Place 2",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(-3.3516036325716203, 2.0709713538312013, -4.616102905047971),
+                name: "News1",
+                description: "News 1",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(4.152938245688819, 2.0371240654684564, -4.40010145738440),
+                name: "News1",
+                description: "News 2",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(-5.835829947684036, 1.128771779134401, -2.1438805290275016),
+                name: "Screen1",
+                description: "Screen 2",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(5.830092643124771, 1.5622032870035916, -2.237158125398845),
+                name: "miniScreen1",
+                description: "Mini Screen 1",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(5.830092643124772, 1.5330604453192185, -0.38702798020061113),
+                name: "miniScreen2",
+                description: "Mini Screen 2",
+                imageUrl: "images/whitePin.png"
+            },
+            {
+                position: new THREE.Vector3(5.830092643124772, 1.518356469188951, 1.5056048123946244),
+                name: "miniScreen3",
+                description: "Mini Screen 3",
+                imageUrl: "images/whitePin.png"
+            },
+        ];
